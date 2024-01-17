61f6efdce6ce3e0e2b3feb074f68de81e7ee95d7
Gabriel Aprilyan <gabrilriu@gmail.com>
2024-01-17 11:48:57 +0700

    final

--- index.js
+++ index.js
@@ -73,3 +73,20 @@ app.put('/products/:id', async(req,res) =>{
         res.status(500).json({message: error.message});
     }
 })
+
+// delete Product
+app.delete('/products/:id', async(req,res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            res.status(404).json({message: `cannot find any product with: ${product}`});
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+})
